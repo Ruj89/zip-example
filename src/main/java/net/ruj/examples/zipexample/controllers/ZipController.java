cabcd9f7c5eaf8cb549aeda040ea0cbd1ffcd409
Ruggero Russo <ruggero.russo89@gmail.com>
2017-04-01 11:45:34 +0200

    java 8 lambdas

--- src/main/java/net/ruj/examples/zipexample/controllers/ZipController.java
+++ src/main/java/net/ruj/examples/zipexample/controllers/ZipController.java
@@ -7,6 +7,9 @@ import net.ruj.examples.zipexample.services.ZipService;
 import java.io.*;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class ZipController {
     private final ZipService zipService;
@@ -28,6 +31,14 @@ public class ZipController {
             addInMemoryFile(virtualFiles, inputStream);
 
             this.zipService.zip(virtualFiles, destination);
+
+            virtualFiles.forEach(vf -> {
+                try {
+                    vf.getInputStream().close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
         }
     }
 
@@ -38,14 +49,27 @@ public class ZipController {
         return bufferRead.readLine();
     }
 
-    private void addInFolderFiles(File[] childFiles, ArrayList<VirtualFile> virtualFiles) throws FileNotFoundException {
-        for (File childFile : childFiles)
-            virtualFiles.add(
-                    new VirtualFile(
-                            new FileInputStream(childFile),
-                            childFile.getName()
-                    )
+    private void addInFolderFiles(File[] childFiles, ArrayList<VirtualFile> appendList) throws FileNotFoundException {
+        appendList.addAll(
+                Arrays.stream(childFiles)
+                        .filter(File::isFile)
+                        .map(this::fileToVirtualFile)
+                        .filter(Objects::nonNull)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    private VirtualFile fileToVirtualFile(File cf) {
+        VirtualFile virtualFile = null;
+        try {
+            virtualFile = new VirtualFile(
+                    new FileInputStream(cf),
+                    cf.getName()
             );
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return virtualFile;
     }
 
     private void addInMemoryFile(ArrayList<VirtualFile> virtualFiles, ByteArrayInputStream inputStream) {
